3bbcdf3b87ba731566beb8736cb0de2c682f2ca5
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-04 02:55:14 +0530

    Add Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+function Home() {
+    return (
+        <Container text>
+            <Header as="h1" content="Test Your Ability here" textAlign="center" />
+        </Container>
+    );
+}
+
+export default Home;
