e5ce8ea695c7a19e3dec56c3cf647cfc7367ff62
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 20:43:10 +0530

    Update student component

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,1 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, Container, Message } from "semantic-ui-react";
+import { useFetchTest } from "../utils/hooks";
 
+function Student() {
+    const [tests, settest, { isLoading, isError }] = useFetchTest();
+    return (
+        <Container text>
+            {!isLoading &&
+                tests.map((test) => {
+                    // if(visibility){
+                        // TODO:ADD CODE FOR visibility
+                    // }
+                    return (
+                        <Card
+                            header={test.name}
+                            key={test._id}
+                            as={Link}
+                            to={`/student/${test._id}`}
+                        ></Card>
+                    );
+                })}
+            {isError && (
+                <Message negative>
+                    <Message.Header>Error while fetching</Message.Header>
+                </Message>
+            )}
+        </Container>
+    );
+}
+
+export default Student;
