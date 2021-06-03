61f842485ade9230d581038bf51d46f4a6bd2311
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 22:10:07 +0530

    Add header in Student page

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Container, Message } from "semantic-ui-react";
+import { Card, Container, Header, Message } from "semantic-ui-react";
 import { useFetchTest } from "../utils/hooks";
 
 function Student() {
     const [tests, settest, { isLoading, isError }] = useFetchTest();
     return (
-        <Container text>
+        <Container>
+            <Header as="h1" textAlign="left">Take a Test</Header>
             {!isLoading &&
                 tests.map((test) => {
                     // if(visibility){
-                        // TODO:ADD CODE FOR visibility
+                    // TODO:ADD CODE FOR visibility
                     // }
                     return (
                         <Card
