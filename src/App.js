5c84b09040f06ca019a092a41f71997d0aeee0af
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 19:46:00 +0530

    add axios baseUrl

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+import "semantic-ui-css/semantic.min.css";
+
+import "./App.css";
+axios.defaults.baseURL = "http://localhost:4040";
+
 function App() {
     return (
         <div className="App">
