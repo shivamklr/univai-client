451127ac825d62b85c12017d51381a216ece5167
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 20:47:32 +0530

    Update Teacher Component

--- src/pages/Teacher.js
+++ src/pages/Teacher.js
@@ -1,11 +1,37 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Container, Card, Message, Button } from "semantic-ui-react";
+import { useFetchTest, useForm } from "../utils/hooks";
 function Teacher() {
     const initialState = {
         testName: "",
     };
     const [postError, setpostError] = useState(false);
     const [postLoading, setpostLoading] = useState(false);
+    const { values, changeHandler, submitHandler } = useForm(
+        callback,
+        initialState
+    );
+    const [tests, settest, { isLoading, isError }] = useFetchTest();
+    async function callback() {
+        try {
+            setpostLoading(true);
+            const res = await axios.post("/test", {
+                name: values.testName,
+            });
+            const newTest = res.data.data.test;
+            settest((prevList) => {
+                return [...prevList, { ...newTest }];
+            });
+            setpostError(false);
+        } catch (error) {
+            setpostError(true);
+        } finally {
+            setpostLoading(false);
+        }
+    }
+    return (
         <Container text>
             {!isLoading &&
                 tests.map((test) => {
@@ -18,6 +44,29 @@ function Teacher() {
                         ></Card>
                     );
                 })}
+            <Form loading={postLoading} noValidate>
+                <Form.Input
+                    label="Test Name"
+                    type="text"
+                    placeholder="Enter Test Name"
+                    name="testName"
+                    value={values.testName}
+                    onChange={changeHandler}
+                />
+                <Button onClick={submitHandler}>Submit</Button>
+            </Form>
+            {isError && (
+                <Message negative>
+                    <Message.Header>Error while fetching</Message.Header>
+                </Message>
+            )}
+            {postError && (
+                <Message negative>
+                    <Message.Header>Error while submitting</Message.Header>
+                </Message>
+            )}
         </Container>
     );
+}
+
 export default Teacher;
