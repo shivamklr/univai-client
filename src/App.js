5678397dafb55539cc3cbea35da418a0cbf1a778
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 19:48:11 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
 
 import "./App.css";
+import Student from "./pages/Student";
+import Teacher from "./pages/Teacher";
+import Test from "./pages/Test";
+
 axios.defaults.baseURL = "http://localhost:4040";
 
 function App() {
     return (
-        <div className="App">
-            <h1> Hey there</h1>
-        </div>
+        <Router>
+            <Route exact path="/student" component={Student} />
+            <Route exact path="/teacher" component={Teacher} />
+            <Route exact path="/teacher/:testId" component={Test} />
+            <Route exact path="/student/:testId" component={Test} />
+        </Router>
     );
 }
 
