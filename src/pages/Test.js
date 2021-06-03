28e6066b177cf909e9b4bf1ee9986b42e0f9b9bd
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-04 01:24:45 +0530

    Add loading component

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Loader} from "semantic-ui-react"
 import SingleTest from "../components/SingleTest";
 import { useFetchTest } from "../utils/hooks";
 
@@ -6,10 +7,13 @@ function Test(props) {
     const pathName = window.location.pathname;
     const userType = pathName.split("/")[1];
     const testId = props.match.params.testId;
-    const [test,settest,{isError, isLoading}]=useFetchTest(testId);
+    const [test, { isError, isLoading }] = useFetchTest(testId);
     return (
         <div>
-            {!isLoading && !isError && <SingleTest userType={userType} {...test}/>}
+            {isLoading && <Loader active={isLoading} />}
+            {!isLoading && !isError && (
+                <SingleTest userType={userType} {...test} />
+            )}
             {isError && <p>Error while fetching test</p>}
         </div>
     );
